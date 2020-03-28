f062233fa949d14b9de4a2b69319f8af6e1d195c
Kostas Bakogiannis <kosbako10@gmail.com>
2020-03-28 14:05:28 +0200

    sw actions

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,8 +1,63 @@
 self.addEventListener('push', event => {
   const data = event.data.json();
   console.log('New notification', data);
+  const { title, text, ...rest } = data;
   const options = {
-    body: data.body
+    body: text,
+    ...rest
   };
-  event.waitUntil(self.registration.showNotification(data.title, options));
+  event.waitUntil(self.registration.showNotification(title, options));
 });
+
+self.addEventListener('notificationclick', function(event) {
+  console.log(event.action);
+  console.log(event);
+  event.notification.close();
+
+  switch (event.action) {
+    case 'google':
+      event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(windowClients => {
+          if (clients.openWindow) {
+            return clients.openWindow('http://www.google.com');
+          }
+        })
+      );
+      break;
+    case 'facebook':
+      event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(windowClients => {
+          if (clients.openWindow) {
+            return clients.openWindow('http://www.facebook.com');
+          }
+        })
+      );
+      break;
+    case 'apple':
+      event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(windowClients => {
+          if (clients.openWindow) {
+            return clients.openWindow('http://www.apple.com');
+          }
+        })
+      );
+      break;
+
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
+});
+
+self.notificationActions = {
+  google: function(customData) {
+    return window.open('http://www.google.com', '_blank');
+  },
+
+  facebook: function(customData) {
+    return window.open('http://www.facebook.com', '_blank');
+  },
+  apple: function(customData) {
+    return window.open('http://www.apple.com', '_blank');
+  }
+};
